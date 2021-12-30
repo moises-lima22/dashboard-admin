d75fa142aaa100d577ac808f876eb388c2366826
Moises Silva <moises.silva2015@outlook.com>
2021-12-29 22:19:29 -0300

    add sidebar

---
+++ dash/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import "./App.css";
+
+import Sidebar from "./components/sidebar/Sidebar";
+
+const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
+
+
+
+  return (
+    <div className="container">
+      <Sidebar sidebarOpen={sidebarOpen} CloseSidebar={closeSidebar}/>
+    </div>
+  );
+};
+
+export default App;
